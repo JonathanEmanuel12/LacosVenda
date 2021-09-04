c023a3f4e73743adbdddd4ebb22ca3356b8348b1
JonathanEmanuel12 <jonathaanemanuel21@gmail.com>
2021-09-04 11:05:40 -0300

    adicionado as variaveis com os dados

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var pergunta1 = {
+    questao = "Mercúrio é um metal que em temperatura ambiente é se mantém no estado líquido. Em qual dos equipamentos/ferramentas abaixo o mercúrio é utilizado?",
+    respostas = ["Tesoura", "Termomêtro", "Picareta", "Óculos", "Borracha"],
+    correto = 1
+};
+
+var pergunta2 = {
+    questao = "Qual dos estados abaixo possui a maior produção de queijo do Brasil?",
+    respostas = ["Minas Gerais", "São Paulo", "Mato Grosso", "Ceará", "Rondônia"],
+    correto = 0
+};
+
+var pergunta3 = {
+    questao = "Qual o santo(a) padroeiro do Brasil?",
+    respostas = ["Santa Teresa de Jesus", "São Pedro", "São Paulo", "Nossa Senhora da Conceição Aparecida", "Santa Luzia"],
+    correto = 2
+};
+
+var pergunta4 = {
+    questao = "Quantas cidade o estado de Minas Gerais possui?",
+    respostas = ["500 cidades", "358 cidades", "652 cidades", "471 cidades", "306 cidades"],
+    correto = 3
+};
+
+var pergunta5 = {
+    questao = "Qual o nome do alimento que possui frango e farinha de trigo em sua composição e que possui a forma de uma gota ",
+    respostas = ["Pizza", "Pastel", "Lasanha", "Strogonoff", "Coxinha"],
+    correto = 4
+};
